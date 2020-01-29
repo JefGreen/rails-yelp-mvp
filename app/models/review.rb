f75d5a6ff161c3520974380772bfab73dcbad20f
Jeff Trempe <trempe.jeff@gmail.com>
2020-01-28 20:48:02 -0500

    Models are setup

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  validates :restaurant, :content, :rating, presence: true
+  validates :rating, inclusion: {
+    in: 0..5,
+    message: '%{value} is not a valid rating'
+  }
+  validates :rating, numericality: { only_integer: true }
+
+  belongs_to :restaurant
+end
